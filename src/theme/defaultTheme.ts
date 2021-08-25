866376882dfeb8b1299943ba079fb79a78b819a2
Marius Sørensen <jobb@s0rensen.no>
2021-08-25 22:13:03 +0200

    Implement landing page and about you design

---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,42 @@
+import { createTheme } from "@material-ui/core";
+
+const defaultTheme = createTheme({
+  typography: {
+    fontFamily: "Lato, Roboto",
+  },
+  palette: {
+    primary: {
+      light: "#D5726A",
+      main: "#C95144",
+    },
+    secondary: {
+      light: "#6B89D3",
+      main: "#283864",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      contained: {
+        color: "#fff",
+        backgroundColor: "#6B89D3",
+        boxShadow: "0px 4px 10px rgba(162, 162, 162, 0.75)",
+        borderRadius: "15px",
+        fontSize: "16px",
+        fontWeight: 300,
+        "&:hover": {
+          backgroundColor: "#283864 !important",
+        },
+        "& .MuiButton-label:hover": {
+          backgroundColor: "transparent",
+        },
+      },
+    },
+  },
+  props: {
+    MuiButton: {
+      variant: "contained",
+    },
+  },
+});
+
+export default defaultTheme;
